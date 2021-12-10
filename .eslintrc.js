6f9e93fa9f8d68717ad61f03dc462cb65b69a91d
hbm <hbmin3789@naver.com>
2021-12-10 13:51:32 +0900

    [INIT] InitProject

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,6 @@ import '@styles/styles.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import {App} from '@components/app/app';
+import App from './components/App';
 
 ReactDom.render(<App />, document.getElementById('root'));
